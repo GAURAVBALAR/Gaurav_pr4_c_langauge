b61a0d03e767bdac284d4fdec590a10c2100e731
unknown <balargaurav471@gmail.com>
2023-06-14 19:15:51 +0100

    First Commit

---
+++ PR(42).C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a,b;
+int c=11;
+clrscr();
+
+for(a=1;b<=5;a++){
+for(b=1;b<=i;b++){
+printf("%d",c);
+c++;
+}
+printf("\n");
+}
+getch();
+}
